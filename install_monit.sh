6b4f7d398f25d728c7bc330041f6ba81dbd3eaed
Jeferson Ortiz <jortiz@AT-SIST-172>
2014-06-25 10:30:59 -0430

    Correccion de errores

--- install_monit.sh
+++ install_monit.sh
@@ -19,12 +19,12 @@ cd /usr/local/etc/
 sudo chmod 777 monitrc
 
 
-sudo sed -e 's/set daemon  60              # check services at 1-minute intervals/set daemon  10/' monitrc.ori > monitrc
-sudo sed -e 's/# set logfile syslog facility log_daemon/set logfile syslog facility log_daemon/' monitrc.ori > monitrc
-sudo sed -e 's/# set pidfile /var/run/monit.pid/set pidfile /var/run/monit.pid/' monitrc.ori > monitrc
-sudo sed -e 's/# set idfile /var/.monit.id/set idfile /var/.monit.id/' monitrc.ori > monitrc
-sudo sed -e 's/# set statefile /var/.monit.state/set statefile /var/.monit.state/' monitrc.ori > monitrc
-sudo sed -e 's/#  include /etc/monit.d/*/include /etc/monit.d/*/' monitrc.ori > monitrc
+sudo sed -e 's/set daemon  60              \# check services at 1-minute intervals/set daemon  10/' monitrc.ori > monitrc
+sudo sed -e 's/\# set logfile syslog facility log_daemon/set logfile syslog facility log_daemon/' monitrc.ori > monitrc
+sudo sed -e 's/\# set pidfile \/var\/ru\n/monit.pid/set pidfile \/var\/run\/monit.pid/' monitrc.ori > monitrc
+sudo sed -e 's/\# set idfile \/var\/.monit.id\/set idfile \/var\/.monit.id/' monitrc.ori > monitrc
+sudo sed -e 's/\# set statefile \/var\/.monit.state/set statefile \/var\/.monit.state/' monitrc.ori > monitrc
+sudo sed -e 's/\#  include \/etc\/monit.d/\*/include \/etc\/monit.d/\*/' monitrc.ori > monitrc
 
 cd $INSTALL_DIR
 
@@ -34,7 +34,7 @@ sudo cp src/monit_init  /etc/init.d/monit
 sudo chmod +x /etc/init.d/monit
 sudo cp src/monit_default /etc/default/monit
 
-sudo mkdir /etc/monit.d
+sudo mkdir /etc/monit.d/
 sudo cp src/mpsanp /etc/monit.d
 sudo cp src/stopScreen.sh /usr/local/bin/
 
